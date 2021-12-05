e604365ea08a46db45f34b77bac26d65f3547387
Tiffany Tam <tamtiffany89@gmail.com>
2021-12-05 16:52:31 -0500

    commit tree 0b0d88e923df8f8127b446fd2c965cc1da80bd4c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  # games.to_json
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games/:id' do
+    # get just 10 of the games from the database
+    # games = Game.all.order(:title).limit(10)
+    game = Game.find(params[:id])
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
